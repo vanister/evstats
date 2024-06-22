97c13a1624f67bca27b4226e144a98eefbf3abd1
vansiter <vanister@users.noreply.github.com>
2024-06-21 21:18:36 -0700

    Updated app and home components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+
+import ExploreContainer from '../components/ExploreContainer';
+import EvsPage from '../components/EvsPage';
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+    <EvsPage title="Home">
+      <ExploreContainer />
+    </EvsPage>
   );
 };
 
