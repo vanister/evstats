7d61df77af723988cb49dff240d077df16a0708c
vansiter <vanister@users.noreply.github.com>
2024-06-27 21:06:43 -0700

    Added capacitor/sqlite

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,30 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'evstats-ionic',
-  webDir: 'dist'
+  appId: 'com.vanister.evs',
+  appName: 'EV Charge Stats',
+  webDir: 'dist',
+  plugins: {
+    CapacitorSQLite: {
+      iosDatabaseLocation: 'Library/CapacitorDatabase',
+      iosIsEncryption: true,
+      iosKeychainPrefix: 'com.vanister.evs',
+      iosBiometric: {
+        biometricAuth: false,
+        biometricTitle: 'Biometric login for capacitor sqlite'
+      },
+      androidIsEncryption: true,
+      androidBiometric: {
+        biometricAuth: false,
+        biometricTitle: 'Biometric login for capacitor sqlite',
+        biometricSubTitle: 'Log in using your biometric'
+      },
+      electronIsEncryption: true,
+      electronWindowsLocation: 'C:\\ProgramData\\CapacitorDatabases',
+      electronMacLocation: '/Volumes/Development_Lacie/Development/Databases',
+      electronLinuxLocation: 'Databases'
+    }
+  }
 };
 
 export default config;
