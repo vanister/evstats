20ff4f5e6d240d328934daf0a842abad947e699d
vansiter <vanister@users.noreply.github.com>
2025-01-14 15:35:29 -0800

    Added a view sql file

---
+++ src/data/sql/ViewSql.ts
@@ -0,0 +1,15 @@
+export const ViewSql = Object.freeze({
+  VehicleChargeSummary: `
+    CREATE VIEW IF NOT EXISTS vw_vehicle_charge_summary AS
+    SELECT 
+      v.id AS vehicle_id, 
+      s.date,
+      s.rate_type_id,
+      s.kwh,
+      r.amount AS rate,
+      s.rate_override
+    FROM sessions s
+      JOIN rate_types r ON r.id = s.rate_type_id
+      JOIN vehicles v ON v.id = s.vehicle_id;
+  `
+});
