0bd52e638b076232338d445724e1d95a704e1381
vansiter <vanister@users.noreply.github.com>
2025-01-30 22:49:24 -0800

    Deleted old seed folder

--- seed/vehicles.sql
+++
@@ -1,7 +0,0 @@
--- Seed vehicles
-INSERT INTO vehicles (year, make, model, trim, battery_size, range, nickname, vin) VALUES
-(2022, 'Ford', 'Mustang Mach-E', 'GT', 91, 270, 'Red Stallion', NULL),
-(2022, 'Tesla', 'Model S', 'Plaid', 100, 400, 'M3', NULL),
-(2023, 'Tesla', 'Model 3', 'Long Range', 78, 334, NULL, NULL),
-(2024, 'Tesla', 'Model X', 'Long Range', 100, 390, NULL, NULL),
-(2025, 'Tesla', 'Model Y', 'Performance', 78, 290, NULL, NULL);
