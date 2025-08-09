b94c1ae9a80dd8549676091fc190db7ad14a93d2
vanister <vanister@users.noreply.github.com>
2025-08-08 19:49:58 -0700

    Deleted unused mocks

--- src/__mocks__/rateData.ts
+++
@@ -1,8 +0,0 @@
-import { RateTypeDbo } from '../models/rateType';
-
-export const MOCK_RATE_TYPES: RateTypeDbo[] = [
-  { id: 1, name: 'Home', amount: 0.12, unit: 'kWh' },
-  { id: 2, name: 'Work', amount: 0.18, unit: 'kWh' },
-  { id: 3, name: 'Other', amount: 0.13, unit: 'kWh' },
-  { id: 4, name: 'DC', amount: 0.32, unit: 'kWh' }
-];
